97fdf3624200da80f64674094d0a53c0270ceda0
JvictorVentura <joaovventura@protonmail.com>
2024-01-05 23:50:40 -0400

    oam fetch sprite

--- ppu.c
+++ ppu.c
@@ -1,5 +1,21 @@
 #include "gameboy.h"
+#include <stdlib.h>
+#include "ppu.h"
 
 void ppu_execute(GameBoy *gb){
 	//	render one line
+}
+
+sprite *fetch_sprite(uint16_t index, GameBoy *gb){
+	if(index >= 0xFE00 && index <= 0xFE9F){		//	check if the index is pointing to OAM
+		sprite *s = (sprite *) malloc(sizeof(sprite)); 	
+		s->y_pos = gb->memory_address[index++];
+		s->x_pos = gb->memory_address[index++];
+		s->sprite_location= gb->memory_address[index++];
+		s->attributes = gb->memory_address[index++];
+
+		return s;
+	}else{
+		return NULL;
+	}
 }
--- ppu.h
+++ ppu.h
@@ -1,1 +1,8 @@
+typedef struct{
+	uint8_t y_pos;
+	uint8_t x_pos;
+	uint8_t sprite_location;
+	uint8_t attributes;
+
+}sprite;
 void ppu_execute();
